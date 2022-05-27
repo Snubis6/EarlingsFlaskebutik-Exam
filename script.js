d68fb6b13494e28a94d9ce8a89529e1738565f53
Snubis6 <snubis9@gmail.com>
2022-05-27 23:54:07 +0200

    s
    d

--- script.js
+++ script.js
@@ -14,13 +14,42 @@ document.getElementById("No").onclick = function () {
     
 };
 
-document.getElementById("newsletter-SubmitButton").onclick = function () {
-  
+
+
+};
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("newsletter-SubmitButton");
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
 }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+
+  }
+}
+btn.addEventListener('click', function handleClick(event) {
+  
+  event.preventDefault();
+
+  const email = document.getElementById('email');
+
+  email.value = '';
+});
 // window.onload = function() {
 //   if (document.getElementsByClassName("Header").style.display === "flex") {
 //     document.getElementsByClassName("Header").style.display = "none";
 //   }
 // };
 
-};
